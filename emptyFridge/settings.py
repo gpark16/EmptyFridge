1b61bb65fa6d2694209f5d0f53d76458cd9ded29
Gregory Park <60415078+gpark16@users.noreply.github.com>
2022-02-24 16:01:37 -0500

    Update settings.py

--- emptyFridge/settings.py
+++ emptyFridge/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dwib##9f5ozqh2hkg=nqe4!)4kw(*o8*nav3udh_ie6h60(bh2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
